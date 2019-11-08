472d03a30fff4abe6f88afa6a464b06797fdf6ec
Eric Li <chang.li@live.com>
2019-11-07 22:17:12 -0500

    Completed Labelling Code with initial labelling completed

---
+++ FaceAPIConfig.py
@@ -0,0 +1,7 @@
+subscription_key = "fa553bd3b27c497a8b1a9ab3ed8cb32b"
+face_api_url = "https://canadacentral.api.cognitive.microsoft.com/face/v1.0/detect"
+
+
+def config():
+    print("call config")
+    return subscription_key, face_api_url
---
+++ FaceLabeling.py
@@ -0,0 +1,87 @@
+import requests
+from PIL import Image
+import os
+import FaceAPIConfig as config
+import json
+from io import BytesIO
+import csv 
+from skimage.transform import resize
+
+def getRectangle(faceDictionary):
+    print("getRectangle",faceDictionary)
+    rect = faceDictionary['faceRectangle']
+    left = rect['left']
+    top = rect['top']
+    bottom = left + rect['height']
+    right = top + rect['width']
+    return (left, top, bottom, right)
+
+with open('ffhq-dataset-v2.json') as json_file:
+    pic_infos = json.load(json_file)
+
+pic_url = pic_infos["0"]["image"]["file_url"]
+
+subscription_key, face_api_url = config.config()
+
+headers = {
+    'Ocp-Apim-Subscription-Key': subscription_key
+}
+
+params = {
+    'returnFaceId': 'true',
+    'returnFaceLandmarks': 'false',
+    'returnFaceAttributes' : 'emotion'
+}
+print("reached")
+
+#used to write the header
+#with open('labels.csv', 'w') as csvFile_header:
+#    writer = csv.writer(csvFile_header)
+#    writer.writerow(['id', 'label'])
+#csvFile_header.close()
+
+
+with open('labels.csv', 'a') as csvFile:   
+    for i in range(0,20): 
+        pic_url = pic_infos[str(i)]["image"]["file_url"]
+
+        response = requests.post(face_api_url, params=params, headers=headers, json={"url": pic_url})
+
+        face_info = response.json() #get face info in dictionary format
+        #get picture from URL
+        pic_response = requests.get(pic_url)
+        img = Image.open(BytesIO(pic_response.content))
+
+        for j, val in enumerate(face_info):
+            rect_coor = getRectangle(val)
+            cropped_img = img.crop(rect_coor)
+
+            cropped_img=cropped_img.resize((64, 64), Image.ANTIALIAS)
+            cropped_img_name = str(i) + "_" + str(j)
+            save_img = cropped_img.save("./cropped_pics/"+cropped_img_name+".jpg")
+            
+            emotion_pred = [val['faceAttributes']["emotion"]['anger'],
+                val['faceAttributes']["emotion"]['happiness'],
+                val['faceAttributes']["emotion"]['neutral'],
+                val['faceAttributes']["emotion"]['sadness'],
+                val['faceAttributes']["emotion"]['surprise'],
+            ]
+
+            #[anger, happiness, neutral, sadness, surprise]
+            max_val = 0
+            for e_i, emotion_val in enumerate(emotion_pred):
+                if(emotion_val > emotion_pred[max_val]):
+                    max_val = e_i
+
+            label = {
+                'id': cropped_img_name,
+                'label': max_val
+            }            
+            wr = csv.DictWriter(csvFile, fieldnames=('id', 'label'), lineterminator = '\n')
+            wr.writerow(label)
+
+
+            
+csvFile.close()
+
+print("finished running")
